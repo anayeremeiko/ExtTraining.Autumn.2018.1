c60be90fb7d105d7b9c85851a2de99eba68da743
anayeremeiko <nastyaeremeyko@gmail.com>
2018-10-29 09:32:03 +0300

    1 task

--- BookLibrary.Tests/Book.Tests.cs
+++ BookLibrary.Tests/Book.Tests.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace BookLibrary.Tests
 {
-    public class Class1
+    [TestFixture]
+    public class BookTests
     {
+        private Book book;
+
+        [OneTimeSetUp]
+        public void CreateBookObject()
+        {
+            book = new Book("C# in Depth", "Jon Skeet", 2019, "Manning", 4, 900, 40);
+        }
+
+        [TestCase("G", ExpectedResult = "Book record: Jon Skeet, C# in Depth, Manning, 2019, 4, 900, $40.000")]
+        [TestCase("F", ExpectedResult = "Book record: Jon Skeet, C# in Depth, Manning, 2019, 4, 900, $40.000")]
+        [TestCase("S", ExpectedResult = "Book record: Jon Skeet, C# in Depth, Manning")]
+        [TestCase("B", ExpectedResult = "Book record: Jon Skeet, C# in Depth")]
+        [TestCase("P", ExpectedResult = "Book record: C# in Depth, Manning")]
+        [TestCase("T", ExpectedResult = "Book record: C# in Depth")]
+        [TestCase(null, ExpectedResult = "Book record: Jon Skeet, C# in Depth, Manning, 2019, 4, 900, $40.000")]
+        [TestCase("   ", ExpectedResult = "Book record: Jon Skeet, C# in Depth, Manning, 2019, 4, 900, $40.000")]
+        public string ToString_ReturnFormatString(string format)
+        {
+            return book.ToString(format);
+        }
+
+        [TestCase("K")]
+        [TestCase("M")]
+        [TestCase(".")]
+        [TestCase("6432")]
+        public void ToString_ThrowFormatException(string format)
+        {
+            Assert.Throws<FormatException>(() => book.ToString(format, CultureInfo.CurrentCulture));
+        }
     }
 }
--- BookLibrary/Book.cs
+++ BookLibrary/Book.cs
@@ -1,13 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BookLibrary
 {
-    public class Book
+    public class Book : IFormattable
     {
-        //TODO
+        private string title;
+        private string author;
+        private string year;
+        private string publishingHouse;
+        private string edition;
+        private string pages;
+        private string price;
+
+        public Book(string title, string author, int year, string publishingHouse, int edition, int pages, decimal price)
+        {
+            Title = title;
+            Author = author;
+            Year = year.ToString();
+            PublishingHouse = publishingHouse;
+            Edition = edition.ToString();
+            Pages = pages.ToString();
+            Price = price.ToString("C3", CultureInfo.CreateSpecificCulture("en-US"));
+        }
+
+        public string Title { get => title; private set => title = value; }
+        public string Author { get => author; private set => author = value; }
+        public string Year { get => year; private set => year = value; }
+        public string PublishingHouse { get => publishingHouse; private set => publishingHouse = value; }
+        public string Edition { get => edition; private set => edition = value; }
+        public string Pages { get => pages; private set => pages = value; }
+        public string Price { get => price; private set => price = value; }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return ToString("G", CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public string ToString(string format)
+        {
+            return ToString(format, CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <param name="format">The format:
+        /// G, F - for full representation,
+        /// S - for short representation,
+        /// B - for info about author and title of the book,
+        /// P - for info about title and publishing house of the book,
+        /// T - for title of the book.</param>
+        /// <param name="formatProvider">The format provider.</param>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        /// <exception cref="FormatException">When the format string is not supported.</exception>
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrWhiteSpace(format))
+            {
+                format = "G";
+            }
+
+            if (formatProvider == null)
+            {
+                formatProvider = CultureInfo.CurrentCulture;
+            }
+
+            switch (format.ToUpperInvariant())
+            {
+                case "G":
+                case "F":
+                    return "Book record: " + Author + ", " + Title + ", " + PublishingHouse + ", " + Year + ", " + Edition + ", " + Pages + ", " + Price;
+                case "S":
+                    return "Book record: " + Author + ", " + Title + ", " + PublishingHouse;
+                case "B":
+                    return "Book record: " + Author + ", " + Title;
+                case "P":
+                    return "Book record: " + Title + ", " + PublishingHouse;
+                case "T":
+                    return "Book record: " + Title;
+                default:
+                    throw new FormatException($"The {format} format string is not supported.");
+            }
+        }
     }
 }
